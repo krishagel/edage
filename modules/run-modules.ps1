0dee8458a580d6f512742303ae1508b40d632a22
Kris Hagel <kris@krishagel.com>
2012-09-05 17:06:56 -0700

    Added new modules and module scheduler
    Google Apps User Creation File Uploads & Lexia File Upload and SFTP
    push.

---
+++ modules/run-modules.ps1
@@ -0,0 +1,20 @@
+﻿<#
+
+.SYNOPSIS
+	Run all of the nightly export modules 
+.DESCRIPTION
+	File launching Kettle data conversions that will perform all necessary exports and conversions for seperate systems.
+.NOTES
+	File Name	:	run-modules.ps1
+	Author		:	Kris Hagel - kris@krishagel.com
+	Date		:	September 5, 2012
+.LINK
+	https://github.com/krishagel/Educational-Data-and-Account-Generation-Engine
+	
+#>
+
+# run Lexia file creation and push
+Z:\data-integration\Kitchen.bat /file:"Z:\Educational-Data-and-Account-Generation-Engine\modules\lexia\Lexia_Job.kjb" :level=debug > Z:\logs\lexia.log
+
+# **TEMP** run google apps export file creation
+Z:\data-integration\Kitchen.bat /file:"Z:\Educational-Data-and-Account-Generation-Engine\modules\google-apps-directory-sync\Google_Job.kjb" :level=debug > Z:\logs\google-apps-file-creation.log
